e068bd766ce9d2cf21df370101b70459471682e4
Christiano0407 <placebo0704@gmail.com>
2020-11-30 09:50:47 -0600

    template

---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+//template
+const Home = () => {
+  const view = ` 
+   <div class="characters">
+   <artcle class="character-item">
+   <a href="#/1/">
+    <img src="image" alt="name">
+    <h2>Name</h2>
+   </a>
+   </artcle>
+   </div>
+  `;
+  return view;
+};
+export default Home;
